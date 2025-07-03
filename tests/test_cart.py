4e52e92c1a844aed011bb5dd9bc8746851730176
callmemek <mtewodros49@gmail.com>
2025-07-04 00:40:54 +0300

    added tests cases for the remove item

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -43,4 +43,44 @@ class TestShoppingCart(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.cart.add_item("Orange", 4, -50.0)
 
-    
+     # Unit tests for remove_item
+    def test_remove_item_partial(self):
+        # Arrange
+        initial_quantity = self.cart.items["candy"]["quantity"]
+        initial_total = self.cart.total
+        
+        # Act
+        self.cart.remove_item("candy", 1)
+        
+        # Assert
+        self.assertEqual(self.cart.items["candy"]["quantity"], initial_quantity - 1)
+        self.assertEqual(self.cart.total, initial_total - (1*1.0))
+
+    def test_remove_item_complete(self):
+        # Arrange
+        initial_total = self.cart.total
+        candy_quantity = self.cart.items["candy"]["quantity"]
+        
+        # Act
+        self.cart.remove_item("candy", candy_quantity)
+        
+        # Assert
+        self.assertNotIn("candy", self.cart.items)
+        self.assertEqual(self.cart.total, initial_total - (candy_quantity*1.0))
+
+    # Edge tests for remove_item
+    def test_remove_nonexistent_item(self):
+        # Arrange/Act/Assert
+        with self.assertRaises(ValueError):
+            self.cart.remove_item("Orange", 1)
+
+    def test_remove_more_than_exists(self):
+        # Arrange/Act/Assert
+        with self.assertRaises(ValueError):
+            self.cart.remove_item("candy", 3)
+
+    def test_remove_zero_quantity(self):
+        # Arrange/Act/Assert
+        with self.assertRaises(ValueError):
+            self.cart.remove_item("candy", 0)
+
